84a9565b699510740aeb1638d09aa484f72f9043
Rahul Garg <rahulgarg17102001@gmail.com>
2025-02-16 14:50:53 +0530

    change

--- Frontend/src/Pages/Riding.jsx
+++ Frontend/src/Pages/Riding.jsx
@@ -19,64 +19,68 @@ const Riding = (props) => {
   });
 
   return (
-    <div className="h-screen">
-      <Link
-        to="/home"
-        className="fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full"
-      >
-        <i className="text-lg font-medium ri-home-5-line"></i>
-      </Link>
-      <div className="h-1/2">
-        <img
-          className="w-full h-full object-cover"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPsQfKVcmFKoAydx-uqkFICDgldRDQcCe1dA&s"
-          alt="Uber logo"
-        />
-      </div>
-      <div className="h-1/2 p-4">
-        <div className="flex items-center justify-between">
+    <div className="h-screen lg:w-[100vw] flex  flex-col items-center bg-gray-100 ">
+      <div className="bg-white ">
+        <Link
+          to="/home"
+          className="fixed right-2 top-2 h-10 w-10  flex items-center justify-center rounded-full"
+        >
+          <i className="text-lg font-medium ri-home-5-line"></i>
+        </Link>
+        <div className="h-[85vw] lg:h-[28vw] bg-white lg:w-[31.5vw] ">
           <img
-            className="h-12"
-            src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg"
-            alt=""
+            className="w-full h-full object-cover"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPsQfKVcmFKoAydx-uqkFICDgldRDQcCe1dA&s"
+            alt="Uber logo"
           />
-          <div className="text-right">
-            <h2 className="text-lg font-medium capitalize">
-              {ride?.captain?.fullName?.firstName +
-                " " +
-                ride.captain.fullName.lastName}
-            </h2>
-            <h4 className="text-[4.5vw] font-normal -mt-1 -mb-1">
-              <span className="text-[5vw] font-semibold"> Plate no- </span>{" "}
-              {ride?.captain?.vehicle?.plate}
-            </h4>
-            {/* <p className="text-sm text-gray-600">Maruti Suzuki Alto</p> */}
-          </div>
         </div>
 
-        <div className="flex gap-2 justify-between flex-col items-center">
-          <div className="w-full mt-5">
-            <div className="flex items-center gap-5 p-3 border-b-2">
-              <i className="text-lg ri-map-pin-2-fill"></i>
-              <div>
-                <h3 className="text-lg font-medium">{ride?.destination}</h3>
-                {/* <p className="text-sm -mt-1 text-gray-600">ahmedabad </p> */}
-              </div>
+        <div className="h-[20vw] p-4  bg-white">
+          <div className="flex items-center justify-between">
+            <img
+              className="h-12"
+              src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg"
+              alt=""
+            />
+            <div className="text-right">
+              <h2 className="text-lg font-medium capitalize">
+                {ride?.captain?.fullName?.firstName +
+                  " " +
+                  ride?.captain?.fullName.lastName}
+              </h2>
+              <h4 className="text-[4.5vw] lg:text-[1.5vw] font-normal -mt-1 -mb-1">
+                <span className="text-[5vw] lg:text-[1.5vw] font-semibold">
+                  {" "}
+                  Plate no-{" "}
+                </span>{" "}
+                {ride?.captain?.vehicle?.plate}
+              </h4>
+              {/* <p className="text-sm text-gray-600">Maruti Suzuki Alto</p> */}
             </div>
-            <div className="flex items-center gap-5 p-3">
-              <i className="ri-currency-line"></i>
-              <div>
-                <h3 className="text-lg font-medium">
-                  {ride?.fare}
-                </h3>
-                <p className="text-sm -mt-1 text-gray-600">Cash</p>
+          </div>
+
+          <div className="flex gap-2 justify-between flex-col items-center">
+            <div className="w-full mt-5">
+              <div className="flex items-center gap-5 p-3 border-b-2">
+                <i className="text-lg ri-map-pin-2-fill"></i>
+                <div>
+                  <h3 className="text-lg font-medium">{ride?.destination}</h3>
+                  {/* <p className="text-sm -mt-1 text-gray-600">ahmedabad </p> */}
+                </div>
+              </div>
+              <div className="flex items-center gap-5 p-3">
+                <i className="ri-currency-line"></i>
+                <div>
+                  <h3 className="text-lg font-medium">{ride?.fare}</h3>
+                  <p className="text-sm -mt-1 text-gray-600">Cash</p>
+                </div>
               </div>
             </div>
           </div>
+          <button className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg">
+            Make a Payment
+          </button>
         </div>
-        <button className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg">
-          Make a Payment
-        </button>
       </div>
     </div>
   );
